5ec1470f78bbe2c8d6c0efb22413a17c521f1fef
kryptocodes <srivatsantb@gmail.com>
2019-04-02 22:56:16 +0530

    updated

--- c/digit.c
+++ c/digit.c
@@ -1,4 +1,4 @@
-//digit->words
+//digittowords
 #include<stdio.h>
 int main()
 {
--- c/studetails.c
+++ c/studetails.c
@@ -6,23 +6,55 @@ struct stu
   int rollno;
   char name[20];
   int marks[5];
-}s;
+}s[10];
+void grad(int b)
+{
+  char g;
+  if(b>=90)
+    printf("O");
+  else if(b>=80)
+    printf("A+");
+  else if(b>=70)
+    printf("A");
+  else if(b>=60)
+    printf("B+");
+  else if(b>=50)
+    printf("B");
+  else
+    printf("F");
+}
 int main()
 {
-  int n;
+  int n,i,j;
   printf("enter no of students");
   scanf("%d",&n);
-  for(int i=0;i<n;i++)
+  for(i=0;i<n;i++)
   {
     printf("\nenter student name:");
-    gets(s.name[i]);
+    //gets(s.name[i]);
+    scanf("%s",s[i].name);
     printf("\nenter rollno:");
-    scanf("%d",&s.rollno);
-    for(int j=1;j<=5;j++)
+    scanf("%d",&s[i].rollno);
+    for(j=1;j<=5;j++)
     {
-      printf("enter marks for subject-%d:");
-      scanf("%d",s.marks[j]);
+      printf("enter marks for subject-%d:",j);
+      scanf("%d",&s[i].marks[j]);
     }
+  }
+
+  printf("\n \t\tMARK SHEEET\n");
+  for(i=0;i<n;i++)
+  {
+    printf("\n\nNAME:%s\t\t\tROLLNO:%d",s[i].name,s[i].rollno);
+    printf("\n----------------------------------------------------\n");
+    printf("\nMarks\t\t\t\tGrades");
+    for(j=1;j<=5;j++)
+    {
+
+      printf("\nsub-%d:%d\t\t\t",j,s[i].marks[j]);
+      grad(s[i].marks[j]);
+    }
+
   }
   return 0;
 }
